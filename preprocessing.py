051f987c67eaa69d4f34fe86a9a0592e7a574039
hUSSEIN <hussienalbared@gmail.com>
2023-07-03 15:50:16 +0200

    change preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -1,5 +1,8 @@
 import csv
 import re
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+from collections import Counter
 
 
 def preprocess_comment(comment):
@@ -45,3 +48,60 @@ with open('preprocessed_data.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     writer.writerow(header)
     writer.writerows(data)
+
+# Create a list to store the comment lengths
+comment_lengths = []
+
+# Iterate through the preprocessed comments and calculate the lengths
+for row in data:
+    comment = row[comment_index]
+    comment_length = len(comment.split())
+    comment_lengths.append(comment_length)
+
+# Plot a histogram of comment lengths
+plt.hist(comment_lengths, bins=20, edgecolor='black')
+plt.xlabel('Comment Length')
+plt.ylabel('Frequency')
+plt.title('Distribution of Comment Lengths')
+plt.show()
+
+
+# Combine all the preprocessed comments into a single string
+all_comments = ' '.join([row[comment_index] for row in data])
+
+# Split the string into individual words
+words = all_comments.split()
+
+# Count the frequency of each word
+word_counts = Counter(words)
+
+# Generate a word cloud from the word counts
+wordcloud = WordCloud(width=800, height=400, background_color='white').generate_from_frequencies(word_counts)
+
+# Display the word cloud
+plt.figure(figsize=(10, 5))
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis('off')
+plt.title('Most Frequent Words')
+plt.show()
+
+
+# Count the number of sarcastic and non-sarcastic comments
+sarcasm_count = 0
+non_sarcasm_count = 0
+for row in data:
+    label = int(row[header.index('label')])
+    if label == 1:
+        sarcasm_count += 1
+    else:
+        non_sarcasm_count += 1
+
+# Create a pie chart to visualize the distribution
+labels = ['Sarcasm', 'Non-sarcasm']
+sizes = [sarcasm_count, non_sarcasm_count]
+colors = ['#ff9999', '#66b3ff']
+plt.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', startangle=90)
+plt.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+plt.title('Distribution of Sarcasm vs. Non-sarcasm')
+plt.show()
+#########
